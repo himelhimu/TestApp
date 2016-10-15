126a583ccde6631272a576633dda1b693f91e16f
Sabbir Ahmed Himel <Sabbir Ahmed Himel>
2016-10-15 16:59:49 +0600

    Initial commit

---
+++ app/src/main/java/com/example/sabbir/testapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sabbir.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testUpload()
+    {
+        int n=0;
+        //Just a dummy to notify myself :D :D 
+    }
+}
